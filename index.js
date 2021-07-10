6de1a8d8eedb12f3830800d8d2c02961860fe3c3
mrudul2019 <mrudulmnair@gmail.com>
2021-07-11 04:27:19 +0530

    split private info

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://godlevel:helloworld@cluster0.iqt0e.mongodb.net/test?retryWrites=true&w=majority',
+// const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user');
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI,
  {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log('DB connected')).catch(err=>console.error(err));
 
+
+app.use(express.urlencoded({extended:true})); 
+app.use(express.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res)=>{
+    const user = new User(req.body);
+    user.save((err, userData)=>{
+        if(err) return res.json({succcess:false, err});
+    })
+    return res.status(200);
+})
+
 app.get('/', (req, res)=>{
-    res.send('hello world');
+    res.send('hello world!');
 });
 
 app.listen(5000);
